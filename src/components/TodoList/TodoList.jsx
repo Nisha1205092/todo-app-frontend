a1f11d1fadc69c056e2771b66f06b53eff7df3d5
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-07-12 13:40:23 +0600

    checkbox connected with DB update

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -7,51 +7,19 @@ import { TodoContext } from '../../contexts/todos.context'
 
 const TodoList = () => {
     const { todosArray } = useContext(TodoContext);
-    console.log({ todosArray })
 
     const randomKeyGenerator = () => {
         const key = Math.floor(Math.random() * 1000000);
-        console.log({ key })
         return key;
     }
     return (
         <TodoListContainer>
             <h3>Todo List</h3>
-
-            {/* 
-            // this works
             {
-                todosArray.length === 0 ?
-                    'loading'
-                    :
-
-                    <>
-                        <TodoListItemContainer key={todosArray[0]._id}>
-                            <CheckBox status={todosArray[0].completed} />
-                            <TodoItem
-                                todoTitle={todosArray[0].title}
-                                todoDescription={todosArray[0].description}
-                            />
-                            <DeleteButton />
-                        </TodoListItemContainer>
-
-                        <TodoListItemContainer key={todosArray[1]._id}>
-                            <CheckBox status={todosArray[1].completed} />
-                            <TodoItem
-                                todoTitle={todosArray[1].title}
-                                todoDescription={todosArray[1].description}
-                            />
-                            <DeleteButton />
-                        </TodoListItemContainer>
-                    </>
-            } */}
-
-            {
-                // this doesn't work
                 todosArray.length !== 0 ?
                     todosArray.map((todo) =>
                         <TodoListItemContainer key={randomKeyGenerator()}>
-                            <CheckBox key={randomKeyGenerator()} status={todo.completed} />
+                            <CheckBox key={randomKeyGenerator()} status={todo.completed} todoId={todo._id} />
                             <TodoItem
                                 key={randomKeyGenerator()}
                                 todoTitle={todo.title}
--- src/components/checkbox/Checkbox.jsx
+++ src/components/checkbox/Checkbox.jsx
@@ -1,11 +1,45 @@
+import { TodoContext } from '../../contexts/todos.context';
 import { CheckBoxContainer } from './CheckBox.styles';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
-const CheckBox = ({ status }) => {
+const CheckBox = ({ status, todoId }) => {
+    const [isChecked, setIsChecked] = useState(status)
+    const { todosArray, setTodosArray } = useContext(TodoContext);
+
+    // useEffect(() => {
+    //     console.log('isChecked: ', isChecked)
+    // }, [isChecked])
+
+    const toggler = (e) => {
+        setIsChecked(e.target.checked)
+
+        const updatedTodos = [...todosArray]; // Create a copy of the todosArray
+
+        const index = updatedTodos.findIndex(todo => todo._id === todoId); // Find the index of the todo item
+        console.log({ index })
+        if (index !== -1) {
+            updatedTodos[index] = {
+                ...updatedTodos[index],
+                completed: e.target.checked, // Modify the status property
+            };
+
+            setTodosArray(updatedTodos); // Update the todosArray state
+            // update in DB
+            fetch(`http://localhost:3000/todos/${todoId}`, {
+                method: 'PUT',
+                body: JSON.stringify({ completed: e.target.checked }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((res) => res.json())
+                .then(data => console.log(data))
+        }
+    }
     return (
         <CheckBoxContainer>
             <div className="cbx">
-                <input id="cbx-12" type="checkbox" defaultChecked={status} />
+                <input id="cbx-12" type="checkbox" checked={isChecked} onChange={toggler} />
                 <label htmlFor="cbx-12"></label>
                 <svg width="15" height="14" viewBox="0 0 15 14" fill="none">
                     <path d="M2 8.36364L6.23077 12L13 2"></path>
