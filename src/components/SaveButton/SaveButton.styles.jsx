ea069528bee67b933da065beb3cd0604f207e7cc
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-07-16 22:01:14 +0600

    changed save icon size

--- src/components/SaveButton/SaveButton.styles.jsx
+++ src/components/SaveButton/SaveButton.styles.jsx
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const SaveButtonContainer = styled.div`
     cursor: pointer;
     & svg {
-        width: 30px;
+        width: 22px;
         height: auto;
         opacity: 0.9;
         color: ${({ theme }) => theme.textAndIcon};
