778d76f9ca7707cad1977c4142e3c1f963e8df64
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-07-17 10:58:30 +0600

    check user preference for theme and use localStorage for saving theme setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css'
 import CreateNewTodo from './components/CreateNewTodo/CreateNewTodo'
-import ThemeToggleButton from './components/ThemeToggleButton/ThemeToggleButton';
 import TodoList from './components/TodoList/TodoList'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme, GlobalStyles } from './theme';
 import ThemeToggler from './components/ThemeToggler/ThemeToggler';
@@ -10,7 +9,22 @@ import ThemeToggler from './components/ThemeToggler/ThemeToggler';
 const App = () => {
   const [theme, setTheme] = useState("light");
   const isDarkTheme = theme === "dark";
-  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
+  const toggleTheme = () => {
+    const updatedTheme = isDarkTheme ? "light" : "dark"
+    setTheme(updatedTheme)
+    localStorage.setItem("theme", updatedTheme)
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia
+      && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (savedTheme && ["dark", "light"].includes(savedTheme)) {
+      setTheme(savedTheme)
+    } else if (prefersDark) {
+      setTheme("dark")
+    }
+  }, [])
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <>
